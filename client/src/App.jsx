b8b7b75314cc2adb79e4fbd685ac81f9f6c2e326
Rohit <rohitranjan2702@gmail.com>
2023-02-25 12:33:32 +0530

    pricing, header added, login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,10 @@ import Home from './page/Home';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Doubt from './page/Doubt';
+import Profile from './page/Profile';
 import SignUp from './page/SignUp';
+import Pricing from './component/Pricing';
+
 function App() {
   return (
   
@@ -14,7 +17,9 @@ function App() {
   <Route path="/" element={<Home/>}/>
   <Route path="/Doubt" element={<Doubt/>}/>
   <Route path="/signup" element={<SignUp/>}/>
+  <Route path="/profile" element={<Profile/>}/>
   </Routes>
+  <Pricing/>
  
   <Footer/>
 
--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import logo from "./assets/logo.png"
 
 const Header = () => {
   return (
@@ -6,9 +7,9 @@ const Header = () => {
     <div class="shadow-lg">
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 ">
 <div class="container flex flex-wrap items-center justify-between mx-auto">
-<a href="https://flowbite.com/" class="flex items-center">
-   <img src="https://flowbite.com/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-   <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+<a href="/" class="flex items-center">
+   <img src="https://res.cloudinary.com/dlmgrochr/image/upload/v1677308464/image-removebg-preview_10_ewg8zc.png" class="h-6 mr-3 sm:h-9" alt="Logo" />
+   <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">CodeHelp</span>
 </a>
 <div class="flex md:order-2">
    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In</button>
