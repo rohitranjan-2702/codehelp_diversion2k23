8ac583668f7a039379b30bde27a6ae2599d15201
BIKRAM-SAHA <bikramlearn15@gmail.com>
2023-02-25 15:57:12 +0530

    Server: added getCallToken route

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-const express=require('express');
-const cors=require('cors');
+const express = require("express");
+const cors = require("cors");
 const errorMiddleware = require("./middleware/errorMiddleware");
 const connection = require("./database/db");
 require("dotenv").config();
-const teacherRoute=require("./routes/teacherRoute");
+const teacherRoute = require("./routes/teacherRoute");
 const userRoute = require("./routes/userRoutes");
+const agoraRoute = require("./routes/agoraRoute");
 
-const port=3001;
+const port = 3001;
 
-const app=express();
+const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-
 app.get("/", (req, res) => {
-    res.send("hello!!!!");
+  res.send("hello!!!!");
 });
 
 app.use("/teacher", teacherRoute);
 app.use("/user", userRoute);
+app.use("/agora", agoraRoute);
 
 app.use(errorMiddleware);
-
 
 connection();
 
