dfb6f722ca6377006bf586e9ee4ea2b10c79b0ad
jradityamishra <106857292+jradityamishra@users.noreply.github.com>
2023-02-25 20:43:42 +0530

    changes done

--- client/src/page/SignUp.jsx
+++ client/src/page/SignUp.jsx
@@ -34,7 +34,7 @@ const Signup = () => {
       redirect: "follow",
     };
 
-    fetch("http://localhost:3001/user/register", requestOptions)
+    fetch("http://localhost:5000/user/register", requestOptions)
       .then((response) => {
         // if (response.status !== 200) {
         //   throw new Error("something went wrong");
@@ -112,24 +112,7 @@ const Signup = () => {
                 password
               </label>
             </div>
-            <div class="group relative z-0 mb-6 w-full flex">
-            <input
-                type="password"
-                name="password"
-                id="floating_pwd"
-                className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
-                placeholder=" "
-                required
-                onChange={(e) => onInputChange(e)}
-              />
-              <label
-                htmlFor="floating_pwd"
-                className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 dark:text-gray-400 peer-focus:dark:text-blue-500"
-              >
-                Password
-              </label>
-             
-            </div>
+           
             <button
               type="submit"
               className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
